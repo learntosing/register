dce10445dff7866d732d702d480c0af2e01f8ecd
Rhys Smoker <r.smoker@gmail.com>
2024-03-03 09:10:35 -0800

    changed verbiage in description

--- src/Description.js
+++ src/Description.js
@@ -12,10 +12,16 @@ function Description() {
 			</div>
 			<h1>Learn to sing with the Mount Baker Toppers!</h1>
 			<p>
-				This group is here to reignite (or spark!) your passion for singing.
-				Fulfill your dream of appearing on stage at the Fairhaven green. Make
-				new friends and harmonize with them. Wave to your friends and family!
-				Rehearsals start May 14th, and the big concert is in June.
+				Join the Mount Baker Toppers and rekindle your love for singing, or
+				elevate your vocal journey to the next stage. Imagine yourself
+				performing onstage at the Fairhaven green, blending voices and creating
+				harmony with fellow music enthusiasts. This is your chance to connect
+				with a community that shares your interest, to learn from each other,
+				and to shine on stage together. Our rehearsals kick off on May 14th,
+				building up to a finale concert in June. We're seeking singers who've
+				dabbled in vocal music before—whether in church choirs, garage bands, or
+				solo performances—and are now looking to take their skills to a communal
+				stage.
 			</p>
 		</div>
 	);
